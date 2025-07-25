73899000f6886bfa264f1d0698511586f3c30091
John Hoffer <8084698+apxep@users.noreply.github.com>
2025-07-25 10:01:53 -0500

    Create linux_prep.bash

---
+++ linux_prep.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo dnf install -y iscsi-initiator-utils device-mapper-multipath git
+sudo systemctl enable --now iscsid
+sudo mpathconf --enable
+sudo systemctl enable --now multipathd
+
+sudo dnf install -y python3-pip python3-setuptools epel-release
+#sudo dnf install -y ansible ansible-core # EPEL versions are old, use PIP
+pip install ansible ansible-core
+pip install pip setuptools --upgrade
+pip install purestorage py-pure-client
