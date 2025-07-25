39480cebc8dd0fa71268f8f9f723a9d550c67a85
John Hoffer <8084698+apxep@users.noreply.github.com>
2025-07-25 11:08:14 -0500

    Update linux_prep.bash

--- linux_prep.bash
+++ linux_prep.bash
@@ -4,10 +4,10 @@ sudo systemctl enable --now iscsid
 sudo mpathconf --enable
 sudo systemctl enable --now multipathd
 
-sudo dnf install -y python3-pip python3-setuptools epel-release
+sudo dnf install -y python3-pip python3-setuptools epel-release sshpass
 #sudo dnf install -y ansible ansible-core # EPEL versions are old, use PIP
 pip install ansible ansible-core
 pip install pip setuptools --upgrade
-pip install purestorage py-pure-client
+pip install purestorage py-pure-client requests-credssp
 pip install "pypsrp<=1.0.0"  # for psrp
 pip install "pywinrm>=0.4.0"  # for winrm
