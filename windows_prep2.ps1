48fa74b0737893bf836a3b1726f9d29faa73323e
John Hoffer <8084698+apxep@users.noreply.github.com>
2025-08-13 10:44:07 -0500

    Update windows_prep2.ps1

--- windows_prep2.ps1
+++ windows_prep2.ps1
@@ -1,8 +1,13 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
+Install-Module -Name SSHSessions -force
+Import-Module SSHSessions
+
 Install-Module -Name PureStoragePowerShellSDK2 -force
+Import-Module PureStoragePowerShellSDK2
 
 Install-Module -Name PureStoragePowerShellToolkit -force
+Import-Module PureStoragePowerShellToolkit
 
 Test-Pfa2WindowsBestPractices -Repair -IncludeIscsi -Force
 
